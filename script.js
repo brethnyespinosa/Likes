fae627d383a0315e4b7e5a23cab0edd31af8e4a5
Brethny Espinosa <brethnyespinosa@gmail.com>
2021-08-23 23:59:38 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// var count = 1;
+
+var count = 9
+var countElement = document.querySelector("#increment")
+
+function addLikes(){
+    count++;
+    countElement.innerText = count + " like(s)";
+    console.log(count);
+}
+
+// var count = 12;
+
+var count2 = 12
+var countElement2 = document.querySelector("#increment2")
+
+function addLikes2(){
+    count2++;
+    countElement2.innerText = count2 + " like(s)";
+    console.log(count2);
+}
+
+var count3 = 9;
+var countElement3 = document.querySelector("#increment3")
+
+function addLikes3(){
+    count3++;
+    countElement3.innerText = count3 + " like(s)";
+    console.log(count3);
+}
